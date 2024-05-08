1576f67b50534449ce7ca1d8db4ffa15e337e8a5
ryo.inoue <user009.dounyu@techacademy.jp>
2024-05-08 11:54:14 +0900

    フラッシュメッセージを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -50,6 +50,8 @@ public class CreateServlet extends HttpServlet {
 
             em.persist(m);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
+            em.close();
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
